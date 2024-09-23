ea2b0ad0aa5a24950167430d378c51b86e806b64
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-09-23 10:34:37 -0400

    merge classes

---
+++ src/main/java/dev/nolij/zume/ZumeBootstrapper.java
@@ -0,0 +1,75 @@
+package dev.nolij.zume;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.impl.gui.FabricGuiEntry;
+import net.fabricmc.loader.impl.gui.FabricStatusTree;
+import dev.nolij.zume.impl.Zume;
+import net.minecraftforge.fml.common.Mod;
+
+import static dev.nolij.zume.impl.ZumeConstants.*;
+
+@Mod(
+	value = MOD_ID,
+	modid = MOD_ID,
+	name = MOD_NAME,
+	version = MOD_VERSION,
+	acceptedMinecraftVersions = VINTAGE_VERSION_RANGE,
+	guiFactory = "dev.nolij.zume.vintage.VintageConfigProvider")
+public class ZumeBootstrapper {
+	
+	@SuppressWarnings("Java8CollectionRemoveIf") // extra lambda = extra method = extra size
+	public static void fabricPreLaunch() {
+		if (ZumeMixinPlugin.ZUME_VARIANT != null)
+			return;
+		
+		String FABRIC_MISSING_DEPENDENCY_MESSAGE = """
+		Failed to detect which variant of Zume to load! Ensure all dependencies are installed:
+		    Fabric (14.4+): Fabric API (fabric-key-binding-api-v1)
+		    Legacy Fabric (6.4-12.2): Legacy Fabric API (legacy-fabric-keybinding-api-v1-common)
+		    Babric (b7.3): Station API (station-keybindings-v0)""";
+		
+		Zume.LOGGER.error(FABRIC_MISSING_DEPENDENCY_MESSAGE);
+		FabricGuiEntry.displayError("Incompatible mods found!", null, tree -> {
+			var tab = tree.addTab("Error");
+			tab.node.addMessage(FABRIC_MISSING_DEPENDENCY_MESSAGE, FabricStatusTree.FabricTreeWarningLevel.ERROR);
+			var itr = tree.tabs.iterator();
+			while (itr.hasNext()) {
+				if (itr.next() != tab)
+					itr.remove();
+			}
+		}, true);
+	}
+	
+	public static void fabricInit() {
+		if (ZumeMixinPlugin.ZUME_VARIANT == null)
+			return;
+		
+		final String className = switch (ZumeMixinPlugin.ZUME_VARIANT) {
+			case ZumeMixinPlugin.MODERN -> "dev.nolij.zume.modern.ModernZume";
+			case ZumeMixinPlugin.LEGACY -> "dev.nolij.zume.legacy.LegacyZume";
+			case ZumeMixinPlugin.PRIMITIVE -> "dev.nolij.zume.primitive.PrimitiveZume";
+			default -> null;
+		};
+		((ClientModInitializer) Class.forName(className).getConstructor().newInstance()).onInitializeClient();
+	}
+	
+	public ZumeBootstrapper() {
+		if (ZumeMixinPlugin.ZUME_VARIANT == null)
+			throw new AssertionError("""
+				Mixins did not load! Zume requires Mixins in order to work properly.
+				Please install one of the following mixin loaders:
+				14.4 - 16.0: MixinBootstrap
+				8.9 - 12.2: MixinBooter >= 5.0
+				7.10 - 12.2: UniMixins >= 0.1.15""");
+		
+		final String className = switch (ZumeMixinPlugin.ZUME_VARIANT) {
+			case ZumeMixinPlugin.LEXFORGE -> "dev.nolij.zume.lexforge.LexZume";
+			case ZumeMixinPlugin.LEXFORGE18 -> "dev.nolij.zume.lexforge18.LexZume18";
+			case ZumeMixinPlugin.LEXFORGE16 -> "dev.nolij.zume.lexforge16.LexZume16";
+			case ZumeMixinPlugin.VINTAGE_FORGE -> "dev.nolij.zume.vintage.VintageZume";
+			default -> null;
+		};
+		Class.forName(className).getConstructor().newInstance();
+	}
+	
+}
